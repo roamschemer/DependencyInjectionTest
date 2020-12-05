b6018101de2d7b51cc63cc99ccb61943b8aecd0e
roamschemer <roamschemer@gmail.com>
2020-12-06 00:49:42 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DependencyInjectionTest {
+
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+
+    public class Person {
+        public string FastName { get; }
+        public string LastName { get; }
+        public Person(string fastName, string lastName) {
+            FastName = fastName;
+            LastName = lastName;
+        }
+        public string FullName() => $"{LastName} {FastName}";
+    }
+
+    public class Vtuber {
+        public Person Person { get; }
+        public string Attribute { get; }
+        public Vtuber(Person person, string attribute) {
+            Person = person;
+            Attribute = attribute;
+        }
+        public string Infomation() => $"{Person.FullName()} Attribute:{Attribute}";
+    }
+
+    public class VTuberGroup {
+
+    }
+}
